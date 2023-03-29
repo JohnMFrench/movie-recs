047ecb42b404c6c67cdf6d7e5ed52db9e265508b
JohnMFrench <johnfrenchiu@gmail.com>
2023-03-29 16:05:22 -0400

    page keeps track of movies users liked or disliked

--- recs-app/pages/grid.tsx
+++ recs-app/pages/grid.tsx
@@ -7,43 +7,85 @@ interface Movie {
     rating: number;
     count: number;
     visible: boolean;
+    closing: boolean;
+}
+
+type UserPrefs = {
+    liked_movies: Movie[];
+    disliked_movies: Movie[];
+}
+
+interface Props {
+    userPrefs: UserPrefs;
+    setUserPrefs: React.Dispatch<React.SetStateAction<UserPrefs>>;
 }
 
 const Grid = () => {
     const num_elements: number = 10;
     let start = 0;
     const [movies, setMovies] = useState<Movie[]>([]);
-
-    /**
-     * Update the visibility of a movie with the given ID to false.
-     * @param movie_id The ID of the movie to update.
-    */
-    function toggleMovieVisibility(movie_id: string) {
-        const updatedMovies = movies.map((movie) => {
-            if (movie.movie_id === movie_id) {
-                return { ...movie, visible: false };
-            } else {
-                return movie;
-            }
-        });
-        setMovies(updatedMovies);
+    const startingPrefs: UserPrefs = {
+        liked_movies: [],
+        disliked_movies: [],
     }
+    const [userPrefs, setUserPrefs] = useState(startingPrefs);
+
+    const toggleMovieClosing = (movie_id: string) => setMovies(movies.map((movie) => movie.movie_id === movie_id ? { ...movie, closing: true } : movie));
+    // const toggleMovieVisibility = (movie_id: string) => setMovies(movies.map((movie) => movie.movie_id === movie_id ? { ...movie, visible: false } : movie));
+    const toggleMovieVisibility = (movie_id: string) => {
+        setMovies(movies.filter((movie) => movie.movie_id !== movie_id));
+    };
+
 
     function onThumbsDownClick(event: React.MouseEvent<HTMLDivElement>, movie_id: string): void {
-        console.log('clicked down movie ' + movie_id);
-        toggleMovieVisibility(movie_id);
+        // console.log('clicked down movie ' + movie_id);
+        toggleMovieClosing(movie_id);
+        const movieToUpdate = movies.find((movie: Movie) => movie.movie_id == movie_id)
+        if (movieToUpdate) {
+            addDislikedMovie(movieToUpdate);
+        }
     }
 
     function onThumbsUpClick(event: React.MouseEvent<HTMLDivElement>, movie_id: string): void {
-        console.log('clicked up movie ' + movie_id)
-        toggleMovieVisibility(movie_id);
+        // console.log('clicked up movie ' + movie_id)
+        toggleMovieClosing(movie_id);
+        const movieToUpdate = movies.find((movie: Movie) => movie.movie_id == movie_id)
+        if (movieToUpdate) {
+            addLikedMovie(movieToUpdate);
+        }
+        console.log('movies = ' + movies.length);
     }
 
     function onNotSeenClick(event: React.MouseEvent<HTMLDivElement>, movie_id: string): void {
-        console.log('clicked not seen ' + movie_id)
-        toggleMovieVisibility(movie_id);
+        // console.log('clicked not seen ' + movie_id)
+        toggleMovieClosing(movie_id);
     }
 
+    function addDislikedMovie(movie: Movie) {
+        let l_m: Movie[] = userPrefs.disliked_movies;
+        if (l_m.indexOf(movie) == -1) {
+            l_m.push(movie);
+        }
+        const updatedUserPrefs: UserPrefs = {
+            ...userPrefs,
+            disliked_movies: l_m,
+        };
+        setUserPrefs(updatedUserPrefs);
+        console.log(userPrefs.disliked_movies);
+    }
+
+    function addLikedMovie(movie: Movie) {
+        let l_m: Movie[] = userPrefs.liked_movies;
+        if (l_m.indexOf(movie) == -1) {
+            l_m.push(movie);
+        }
+        const updatedUserPrefs: UserPrefs = {
+            ...userPrefs,
+            liked_movies: l_m,
+        };
+        setUserPrefs(updatedUserPrefs);
+        console.log(userPrefs.liked_movies);
+    }
     /**
      * Fetch top rated movies data from a JSON file and update the state of `movies`
      * to contain an array of movies with their movie_id, name, rating, count, and visible properties.
@@ -51,7 +93,7 @@ const Grid = () => {
     useEffect(() => {
         const fetchMovies = async () => {
             // Fetch data from a JSON file containing top rated movies.
-            const response = await fetch('/top_rated_movies30.json');
+            const response = await fetch('/top_rated_50.json');
             const data = await response.json();
 
             // Create an array of movies from the fetched data.
@@ -62,6 +104,7 @@ const Grid = () => {
                 rating: movie.Rating,
                 count: movie.Count,
                 visible: true,
+                closing: false,
             }));
 
             // Update the state of `movies` with the array of movies.
@@ -83,7 +126,7 @@ const Grid = () => {
                     movies.map((movie: Movie, i: number) => (
                         <>
                             {movie.visible &&
-                                <div key={i} className="movie-container">
+                                <div key={i} className={`movie-container ${movie.closing ? "closed" : ""}`} onAnimationEnd={() => toggleMovieVisibility(movie.movie_id)} >
                                     <div className="image-container">
                                         <img
                                             src={'/' + movie.movie_id + '.jpg'}
