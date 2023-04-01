dbaa2b1822c1f1b05744e216d13b4ecedb21939c
JohnMFrench <johnfrenchiu@gmail.com>
2023-04-01 09:26:42 -0400

    setUpClass

--- tests/test_api_app.py
+++ tests/test_api_app.py
@@ -1,16 +1,15 @@
 import sys 
 print(sys.path)
 from api.app import create_app
-# from api.app import create_app
 import unittest
 import requests
 
 class TestApiApp(unittest.TestCase):
     def setUpClass(self) -> None:
-        super().setUp()
         # instantiate copy of app to run tests
         self.app = create_app()
         self.base_url = 'http://localhost:5000/api'
+        return super().setUpClass()
 
     def test_movie_genre_endpoint(self):
         url = self.base_url + '/movies/1'
