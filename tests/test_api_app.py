096e8629d9b6e76361b2884d7085978e45f47622
JohnMFrench <johnfrenchiu@gmail.com>
2023-04-01 11:26:49 -0400

    test context

--- tests/test_api_app.py
+++ tests/test_api_app.py
@@ -10,6 +10,7 @@ class TestApiApp(unittest.TestCase):
         # instantiate copy of app to run tests
         self.app = create_app()
         self.base_url = 'http://127.0.0.1:5000/api'
+        print(type(self.app), self.app)
         return super().setUpClass()
 
     def test_movie_genre_endpoint(self):
