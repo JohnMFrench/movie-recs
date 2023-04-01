46b868c7028827c010bb49822624bc51bdb7e7ca
JohnMFrench <johnfrenchiu@gmail.com>
2023-04-01 13:58:28 -0400

    fixed animation issue with className

--- recs-app/components/MovieContainer/movie_container.component.tsx
+++ recs-app/components/MovieContainer/movie_container.component.tsx
@@ -1,69 +1,89 @@
-import React, { useEffect, useState } from 'react';
-import styles from './movie_container.module.css';
-import Movie from './movie.type';
-import ButtonContainer from '../ButtonContainer/button_container.component';
-import GenreDataService from '@/api/movies.api';
+import React, { useEffect, useState } from "react";
+import styles from "./movie_container.module.css";
+import Movie from "./movie.type";
+import ButtonContainer from "../ButtonContainer/button_container.component";
+import GenreDataService from "@/api/movies.api";
 
 interface MovieContainerProps {
-    movie: Movie;
-    onThumbsDownClick(event: React.MouseEvent<HTMLDivElement>, movie_id: string): void;
-    onThumbsUpClick(event: React.MouseEvent<HTMLDivElement>, movie_id: string): void;
-    onNotSeenClick(event: React.MouseEvent<HTMLDivElement>, movie_id: string): void;
-    toggleMovieVisibility(movie_id: string): void;
+  movie: Movie;
+  onThumbsDownClick(
+    event: React.MouseEvent<HTMLDivElement>,
+    movie_id: string
+  ): void;
+  onThumbsUpClick(
+    event: React.MouseEvent<HTMLDivElement>,
+    movie_id: string
+  ): void;
+  onNotSeenClick(
+    event: React.MouseEvent<HTMLDivElement>,
+    movie_id: string
+  ): void;
+  toggleMovieVisibility(movie_id: string): void;
 }
 
 function MovieContainer(props: MovieContainerProps) {
-    const [genres, setGenres] = useState<string[]>([]);
+  const [genres, setGenres] = useState<string[]>([]);
 
-    const handleImageError = (event: React.SyntheticEvent<HTMLImageElement>) => {
-        const imgElement = event.currentTarget;
-        imgElement.style.display = 'none';
-    }
+  const handleImageError = (event: React.SyntheticEvent<HTMLImageElement>) => {
+    const imgElement = event.currentTarget;
+    imgElement.style.display = "none";
+  };
 
+  useEffect(() => {
+    const genreDataService = new GenreDataService();
+    genreDataService
+      .getGenres(Number.parseInt(props.movie.movie_id))
+      .then((response) => {
+        setGenres(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    useEffect(() => {
-        const genreDataService = new GenreDataService();
-        genreDataService.getGenres(Number.parseInt(props.movie.movie_id))
-            .then((response) => {
-                setGenres(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
-
-    return (
-        <div
-            key={props.movie.movie_id}
-            className={styles.movieContainer + ` ${props.movie.closing ? "closed" : ""}`} onAnimationEnd={() => props.toggleMovieVisibility(props.movie.movie_id)}>
-            <div className={styles.imageContainer}>
-                <img
-                    src={'https://johnmfrench-movie-recs-public-posters.s3.amazonaws.com/public/' + props.movie.movie_id + '.jpg'}
-                    alt=""
-                    onError={handleImageError}
-                    style={{ objectFit: 'cover', width: '100%', height: '100%' }}
-                    className={styles.movieImage}
-                />
-                <div className={styles.overlay}>
-                    <h2 className={styles.movieContainerTitle}>
-                        {props.movie.name}
-                    </h2>
-                    <h2 className={styles.movieContainerGenres}>
-                        {/* <em> */}
-                            {genres}
-                        {/* </em> */}
-                    </h2>
-                    <ButtonContainer
-                        movie={props.movie}
-                        onNotSeenClick={(e: React.MouseEvent<HTMLDivElement>) => props.onNotSeenClick(e, props.movie.movie_id)}
-                        onThumbsDownClick={(e: React.MouseEvent<HTMLDivElement>) => props.onThumbsDownClick(e, props.movie.movie_id)}
-                        onThumbsUpClick={(e: React.MouseEvent<HTMLDivElement>) => props.onThumbsUpClick(e, props.movie.movie_id)}
-                    />
-                </div>
-            </div>
+  return (
+    <div
+      key={props.movie.movie_id}
+      className={
+        styles.movieContainer + ` ${props.movie.closing ? styles.closed : ""}`
+      }
+      onAnimationEnd={() => props.toggleMovieVisibility(props.movie.movie_id)}
+    >
+      <div className={styles.imageContainer}>
+        <img
+          src={
+            "https://johnmfrench-movie-recs-public-posters.s3.amazonaws.com/public/" +
+            props.movie.movie_id +
+            ".jpg"
+          }
+          alt=""
+          onError={handleImageError}
+          style={{ objectFit: "cover", width: "100%", height: "100%" }}
+          className={styles.movieImage}
+        />
+        <div className={styles.overlay}>
+          <h2 className={styles.movieContainerTitle}>{props.movie.name}</h2>
+          <h2 className={styles.movieContainerGenres}>
+            {/* <em> */}
+            {genres}
+            {/* </em> */}
+          </h2>
+          <ButtonContainer
+            movie={props.movie}
+            onNotSeenClick={(e: React.MouseEvent<HTMLDivElement>) =>
+              props.onNotSeenClick(e, props.movie.movie_id)
+            }
+            onThumbsDownClick={(e: React.MouseEvent<HTMLDivElement>) =>
+              props.onThumbsDownClick(e, props.movie.movie_id)
+            }
+            onThumbsUpClick={(e: React.MouseEvent<HTMLDivElement>) =>
+              props.onThumbsUpClick(e, props.movie.movie_id)
+            }
+          />
         </div>
-
-    );
+      </div>
+    </div>
+  );
 }
 
 export default MovieContainer;
--- recs-app/pages/grid.tsx
+++ recs-app/pages/grid.tsx
@@ -1,180 +1,208 @@
-import React, { useState, useEffect, SyntheticEvent } from 'react';
-import HelpBar from '@/components/HelpBar/helpbar.component';
-import Navbar from '@/components/NavBar/navbar.component';
-import ButtonContainer from '@/components/ButtonContainer/button_container.component';
-import Toast from '@/components/Toast/toast.component';
-import Modal from '@/components/Modal/modal.component';
-import Movie from '@/components/MovieContainer/movie.type';
-import MovieContainer from '@/components/MovieContainer/movie_container.component';
-import ToastProps from '@/components/Toast/toast.type';
+import React, { useState, useEffect, SyntheticEvent } from "react";
+import HelpBar from "@/components/HelpBar/helpbar.component";
+import Navbar from "@/components/NavBar/navbar.component";
+import ButtonContainer from "@/components/ButtonContainer/button_container.component";
+import Toast from "@/components/Toast/toast.component";
+import Modal from "@/components/Modal/modal.component";
+import Movie from "@/components/MovieContainer/movie.type";
+import MovieContainer from "@/components/MovieContainer/movie_container.component";
+import ToastProps from "@/components/Toast/toast.type";
 
 type UserPrefs = {
-    liked_movies: Movie[];
-    disliked_movies: Movie[];
-}
+  liked_movies: Movie[];
+  disliked_movies: Movie[];
+};
 
 interface Props {
-    userPrefs: UserPrefs;
-    setUserPrefs: React.Dispatch<React.SetStateAction<UserPrefs>>;
+  userPrefs: UserPrefs;
+  setUserPrefs: React.Dispatch<React.SetStateAction<UserPrefs>>;
 }
 
 const Grid = () => {
-    const defaultMoviesShown: number = 20
-    const [movies, setMovies] = useState<Movie[]>([]);
-    const startingPrefs: UserPrefs = {
-        liked_movies: [],
-        disliked_movies: [],
-    }
-    const [userPrefs, setUserPrefs] = useState(startingPrefs);
-    const [moviesShown, setMoviesShown] = useState(defaultMoviesShown);
-    const [toastMessage, setToastMessage] = useState('');
-    const [modalVisible, setModalVisible] = useState(false);
-    type movieOrNull = Movie | null;
-    const [modalFocusedMovie, setModalFocusedMovie] = useState<movieOrNull>(null);
-    const [toastRecButtonVisible, setToastRecButtonVisible] = useState(false);
+  const defaultMoviesShown: number = 20;
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const startingPrefs: UserPrefs = {
+    liked_movies: [],
+    disliked_movies: [],
+  };
+  const [userPrefs, setUserPrefs] = useState(startingPrefs);
+  const [moviesShown, setMoviesShown] = useState(defaultMoviesShown);
+  const [toastMessage, setToastMessage] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+  type movieOrNull = Movie | null;
+  const [modalFocusedMovie, setModalFocusedMovie] = useState<movieOrNull>(null);
+  const [toastRecButtonVisible, setToastRecButtonVisible] = useState(false);
 
-    const toggleMovieClosing = (movie_id: string) => setMovies(movies.map((movie) => movie.movie_id === movie_id ? { ...movie, closing: true } : movie));
-    // const toggleMovieVisibility = (movie_id: string) => setMovies(movies.map((movie) => movie.movie_id === movie_id ? { ...movie, visible: false } : movie));
-    const toggleMovieVisibility = (movie_id: string) => {
-        setMovies(movies.filter((movie) => movie.movie_id !== movie_id));
+  const toggleMovieClosing = (movie_id: string) =>
+    setMovies(
+      movies.map((movie) =>
+        movie.movie_id == movie_id ? { ...movie, closing: true } : movie
+      )
+    );
+  const toggleMovieVisibility = (movie_id: string) => {
+    setMovies(movies.filter((movie) => movie.movie_id !== movie_id));
+  };
+
+  const handleImageError = (event: React.SyntheticEvent<HTMLImageElement>) => {
+    const imgElement = event.currentTarget;
+    imgElement.style.display = "none";
+  };
+
+  function onThumbsDownClick(
+    event: React.MouseEvent<HTMLDivElement>,
+    movie: Movie
+  ): void {
+    toggleMovieClosing(movie.movie_id);
+    setTimeout(() => {
+      toggleMovieVisibility(movie.movie_id);
+    }, 500);
+    const movieToUpdate = movies.find(
+      (movie: Movie) => movie.movie_id == movie.movie_id
+    );
+    if (movieToUpdate) {
+      addDislikedMovie(movieToUpdate);
+    }
+  }
+
+  function updateToastMessage() {
+    const numMoviesLiked: number =
+      userPrefs.liked_movies.length + userPrefs.disliked_movies.length;
+    setToastMessage(numMoviesLiked.toString());
+  }
+
+  function onThumbsUpClick(
+    event: React.MouseEvent<HTMLDivElement>,
+    movie_id: string
+  ): void {
+    // console.log('clicked up movie ' + movie_id)
+    toggleMovieClosing(movie_id);
+    const movieToUpdate = movies.find(
+      (movie: Movie) => movie.movie_id == movie_id
+    );
+    if (movieToUpdate) {
+      addLikedMovie(movieToUpdate);
+    }
+    console.log("movies = " + movies.length);
+  }
+
+  function onNotSeenClick(
+    event: React.MouseEvent<HTMLDivElement>,
+    movie: Movie
+  ): void {
+    // console.log('clicked not seen ' + movie_id)
+    // toggleMovieClosing(movie_id);
+    setModalVisible(true);
+    setModalFocusedMovie(movie);
+  }
+
+  function addDislikedMovie(movie: Movie) {
+    //push this disliked movie to the array of liked_movies
+    // in userPrefs if it not already in
+    let l_m: Movie[] = userPrefs.disliked_movies;
+    if (l_m.indexOf(movie) == -1) {
+      l_m.push(movie);
+    }
+    const updatedUserPrefs: UserPrefs = {
+      ...userPrefs,
+      disliked_movies: l_m,
     };
+    setUserPrefs(updatedUserPrefs);
+    updateToastMessage();
+    console.log(userPrefs.disliked_movies);
+  }
 
-    const handleImageError = (event: React.SyntheticEvent<HTMLImageElement>) => {
-        const imgElement = event.currentTarget;
-        imgElement.style.display = 'none';
+  function addLikedMovie(movie: Movie) {
+    //push this liked movie to the array of liked_movies
+    // in userPrefs if it not already in
+    let l_m: Movie[] = userPrefs.liked_movies;
+    if (l_m.indexOf(movie) == -1) {
+      l_m.push(movie);
     }
-
-    function onThumbsDownClick(event: React.MouseEvent<HTMLDivElement>, movie_id: string): void {
-        // console.log('clicked down movie ' + movie_id);
-        toggleMovieClosing(movie_id);
-        const movieToUpdate = movies.find((movie: Movie) => movie.movie_id == movie_id)
-        if (movieToUpdate) {
-            addDislikedMovie(movieToUpdate);
-        }
+    const updatedUserPrefs: UserPrefs = {
+      ...userPrefs,
+      liked_movies: l_m,
+    };
+    setUserPrefs(updatedUserPrefs);
+    updateToastMessage();
+    // setModalVisible(true);
+    if (l_m.length >= 5) {
+      setToastRecButtonVisible(true);
     }
+    console.log(userPrefs.liked_movies);
+  }
 
-    function updateToastMessage() {
-        const numMoviesLiked: number = userPrefs.liked_movies.length + userPrefs.disliked_movies.length;
-        setToastMessage(numMoviesLiked.toString());
+  function onWindowScroll(e: any) {
+    const scrollTop = document.documentElement.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const clientHeight = document.documentElement.clientHeight;
+    if (scrollTop + clientHeight >= (scrollHeight * 2) / 3) {
+      setMoviesShown((prevMoviesShown) => prevMoviesShown + 10);
     }
+  }
+  /**
+   * Fetch top rated movies data from a JSON file and update the state of `movies`
+   * to contain an array of movies with their movie_id, name, rating, count, and visible properties.
+   */
+  useEffect(() => {
+    const fetchMovies = async () => {
+      // Fetch data from a JSON file containing top rated movies.
+      const response = await fetch("/top_rated_movies500.json");
+      const data = await response.json();
 
-    function onThumbsUpClick(event: React.MouseEvent<HTMLDivElement>, movie_id: string): void {
-        // console.log('clicked up movie ' + movie_id)
-        toggleMovieClosing(movie_id);
-        const movieToUpdate = movies.find((movie: Movie) => movie.movie_id == movie_id)
-        if (movieToUpdate) {
-            addLikedMovie(movieToUpdate);
-        }
-        console.log('movies = ' + movies.length);
-    }
+      // Create an array of movies from the fetched data.
+      const movieArray: unknown[] = Object.values(data);
+      const updatedMovieArray = movieArray.map((movie: any, index) => ({
+        movie_id: Object.keys(data)[index],
+        name: movie.title,
+        rating: movie.Rating,
+        count: movie.Count,
+        visible: true,
+        closing: false,
+      }));
 
-    function onNotSeenClick(event: React.MouseEvent<HTMLDivElement>, movie: Movie): void {
-        // console.log('clicked not seen ' + movie_id)
-        // toggleMovieClosing(movie_id);
-        setModalVisible(true);
-        setModalFocusedMovie(movie);
-    }
+      // Update the state of `movies` with the array of movies.
+      setMovies(updatedMovieArray);
+    };
+    window.addEventListener("scroll", (e) => onWindowScroll(e));
 
-    function addDislikedMovie(movie: Movie) {
-        //push this disliked movie to the array of liked_movies
-        // in userPrefs if it not already in
-        let l_m: Movie[] = userPrefs.disliked_movies;
-        if (l_m.indexOf(movie) == -1) {
-            l_m.push(movie);
-        }
-        const updatedUserPrefs: UserPrefs = {
-            ...userPrefs,
-            disliked_movies: l_m,
-        };
-        setUserPrefs(updatedUserPrefs);
-        updateToastMessage();
-        console.log(userPrefs.disliked_movies);
-    }
+    // Call the `fetchMovies` function when the component mounts.
+    fetchMovies();
+  }, []);
 
-    function addLikedMovie(movie: Movie) {
-        //push this liked movie to the array of liked_movies
-        // in userPrefs if it not already in
-        let l_m: Movie[] = userPrefs.liked_movies;
-        if (l_m.indexOf(movie) == -1) {
-            l_m.push(movie);
-        }
-        const updatedUserPrefs: UserPrefs = {
-            ...userPrefs,
-            liked_movies: l_m,
-        };
-        setUserPrefs(updatedUserPrefs);
-        updateToastMessage();
-        // setModalVisible(true);
-        if (l_m.length >= 5) {
-            setToastRecButtonVisible(true);
-        }
-        console.log(userPrefs.liked_movies);
-    }
-
-    function onWindowScroll(e: any) {
-        const scrollTop = document.documentElement.scrollTop;
-        const scrollHeight = document.documentElement.scrollHeight;
-        const clientHeight = document.documentElement.clientHeight;
-        if (scrollTop + clientHeight >= (scrollHeight * 2) / 3) {
-            setMoviesShown(prevMoviesShown => prevMoviesShown + 10);
-        }
-    }
-    /**
-     * Fetch top rated movies data from a JSON file and update the state of `movies`
-     * to contain an array of movies with their movie_id, name, rating, count, and visible properties.
-     */
-    useEffect(() => {
-        const fetchMovies = async () => {
-            // Fetch data from a JSON file containing top rated movies.
-            const response = await fetch('/top_rated_movies500.json');
-            const data = await response.json();
-
-            // Create an array of movies from the fetched data.
-            const movieArray: unknown[] = Object.values(data);
-            const updatedMovieArray = movieArray.map((movie: any, index) => ({
-                movie_id: Object.keys(data)[index],
-                name: movie.title,
-                rating: movie.Rating,
-                count: movie.Count,
-                visible: true,
-                closing: false,
-            }));
-
-            // Update the state of `movies` with the array of movies.
-            setMovies(updatedMovieArray);
-        };
-        window.addEventListener('scroll', (e) => onWindowScroll(e));
-
-        // Call the `fetchMovies` function when the component mounts.
-        fetchMovies();
-    }, []);
-
-
-    return (
-        <>
-            <Navbar title={'MovieLens Recommendations'} />
-            <HelpBar />
-            <div className="app-container">
-                {
-                    movies.slice(0, moviesShown).map((movie: Movie, i: number) => (
-                        <>
-                            {movie.visible &&
-                                <MovieContainer movie={movie}
-                                    onThumbsDownClick={onThumbsDownClick}
-                                    onThumbsUpClick={onThumbsUpClick}
-                                    onNotSeenClick={(e: React.MouseEvent<HTMLDivElement>) => onNotSeenClick(e, movie)}
-                                    toggleMovieVisibility={toggleMovieVisibility} />
-                            }
-                            <Toast message={toastMessage} recButtonVisible={toastRecButtonVisible} />
-                        </>
-                    ))
+  return (
+    <>
+      <Navbar title={"MovieLens Recommendations"} />
+      <HelpBar />
+      <div className="app-container">
+        {movies.slice(0, moviesShown).map((movie: Movie, i: number) => (
+          <>
+            {movie.visible && (
+              <MovieContainer
+                movie={movie}
+                onThumbsDownClick={(e) => onThumbsDownClick(e, movie)}
+                onThumbsUpClick={onThumbsUpClick}
+                onNotSeenClick={(e: React.MouseEvent<HTMLDivElement>) =>
+                  onNotSeenClick(e, movie)
                 }
-                <Modal visible={modalVisible} movie={modalFocusedMovie} onClose={function (): void {
-                    setModalVisible(false);
-                }} />
-            </div>
-        </>
-    )
-}
+                toggleMovieVisibility={toggleMovieVisibility}
+              />
+            )}
+            <Toast
+              message={toastMessage}
+              recButtonVisible={toastRecButtonVisible}
+            />
+          </>
+        ))}
+        <Modal
+          visible={modalVisible}
+          movie={modalFocusedMovie}
+          onClose={function (): void {
+            setModalVisible(false);
+          }}
+        />
+      </div>
+    </>
+  );
+};
 
-export default Grid
+export default Grid;
