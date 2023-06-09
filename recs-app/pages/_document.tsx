ab8f399917787e0d9e91a716ec1fe83468d0d478
JohnMFrench <johnfrenchiu@gmail.com>
2023-06-09 14:54:48 -0400

    build issues

--- recs-app/pages/_document.tsx
+++ recs-app/pages/_document.tsx
@@ -1,31 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import * as gtag from '../components/Analytics/gtag'
-import Script from 'next/script'
 
 export default function Document() {
-  gtag.pageview('home'); // Call the function here before the return statement
-  
   return (
     <Html lang="en">
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <Head />
       {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
       <body>
         <Main />
         <NextScript />
